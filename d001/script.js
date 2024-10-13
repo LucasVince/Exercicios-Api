d8ec0e051b1a2ecec7a7ae05c2151b883cd6598a
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-10-13 18:02:59 -0300

    Add files via upload

---
+++ d001/script.js
@@ -0,0 +1,29 @@
+const btnConverter = document.querySelector('#converter');
+const selectMoeda = document.querySelector('#moeda');
+
+btnConverter.addEventListener('click', () => {
+    if (selectMoeda.value == '') {
+        alert('Por favor, selecione uma moeda.');
+        return;
+    } else {
+        const targetCurrency = selectMoeda.value;
+
+        const apiKey = '9e5df7712d065cad18e41efdd8d805c4';
+        const url = `http://api.currencylayer.com/live?access_key=${apiKey}&currencies=${targetCurrency}`;
+
+        const convertFunc = async () => {
+            const response = await fetch(url);
+            const data = await response.json();
+            const rate = data.quotes[`USD${targetCurrency}`];
+
+            const text = document.querySelector('p');
+
+            text.innerText = `1 $USD = $${rate.toFixed(2)} ${targetCurrency}`;
+
+            console.log(data);
+            console.log(data.quotes);
+        }
+
+        convertFunc();
+    }
+});
